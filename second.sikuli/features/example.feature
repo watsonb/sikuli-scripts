fdb65a778e866703bfed4e305b2ac6c4f5537363
Jenkins slave user <jenkins@rtwbui-pdsi-client1.mitre.org>
2015-06-09 09:27:59 -0500

    adding scenario to actually login

--- second.sikuli/features/example.feature
+++ second.sikuli/features/example.feature
@@ -1,7 +1,14 @@
 Feature: Login
   
-  Scenario: Login 
+  Scenario: Access Login Page
     Given I have launched Google Chrome
     And I wait at least 20 seconds for "chrome_open2.png" to appear
     When I type "http://devops-devapp/admin"
     Then I should see "devapp-admin.png"
+
+  Scenario: Log into the application
+    Given The login page is visible
+    When I type "jenkins" into the username field
+      And I type "password" into the password field
+      And I click the "btn_login.png" button
+    Then I should see "txt_welcome_jenkins.png"
--- second.sikuli/features/step_definitions/example_steps.rb
+++ second.sikuli/features/step_definitions/example_steps.rb
@@ -21,3 +21,28 @@ end
 Given /^I wait at least (\d+) seconds for "([^"]*)" to appear$/ do |time, filename|
   @screen.wait filename, time.to_i
 end
+
+Given /^The login page is visible$/ do
+  @screen.find "devapp-admin.png"
+end
+
+When /^I type "([^"]*)" into the username field$/ do |text|
+  @screen.click "fld_username.png"
+  @screen.type text
+  #@screen.type Sikuli::KEY_RETURN
+end
+
+And /^I type "([^"]*)" into the password field$/ do |text|
+  @screen.click "fld_password.png"
+  @screen.type text
+end
+
+And /^I click the "([^"]*)" button$/ do |filename|
+  @screen.click filename
+end
+
+#Then /^I should see "([^"]*)"$/ do |filename|
+  #@screen.find filename
+#  @screen.wait filename, 5
+#end
+
